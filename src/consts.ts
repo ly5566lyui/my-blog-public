71e4df6af3f89c04cdee4567429d10b3926c169e
Long Yi <2667317838@qq.com>
2026-08-05 02:02:02 +0800

    将GitHub配置改为个人仓库

--- src/consts.ts
+++ src/consts.ts
@@ -3,14 +3,13 @@ export const ANIMATION_DELAY = 0.1
 export const CARD_SPACING = 36
 export const CARD_SPACING_SM = 24
 export const BLOG_SLUG_KEY = process.env.BLOG_SLUG_KEY || ''
-
 /**
  * GitHub 仓库配置
  */
 export const GITHUB_CONFIG = {
-	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'yysuni',
-	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
-	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-',
-	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
+OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'ly5566lyui',
+REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'my-blog-public',
+BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
+APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-',
+ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
